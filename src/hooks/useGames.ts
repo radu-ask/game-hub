0f593a3b21de76599984aa9c29cc1d932003c6c3
Radu Castravet <r.castravet@codat.io>
2023-10-29 11:33:19 +0000

    add critic scores

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -7,10 +7,18 @@ interface FetchGamesResponse {
   results: Game[];
 }
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
 const useGames = () => {
