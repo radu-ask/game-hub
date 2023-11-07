2cbfceb68021f4187207c4f61bb0d5c8d2bce0d1
Radu Castravet <r.castravet@codat.io>
2023-11-07 21:27:43 +0000

    add no image handling

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImagePlaceholer from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
-    if (!url) return "";
+    if (!url) return noImagePlaceholer;
     const target = 'media/';
     const index = url.indexOf(target) + target.length;
     return url.slice(0, index) + "crop/600/400/" + url.slice(index);
